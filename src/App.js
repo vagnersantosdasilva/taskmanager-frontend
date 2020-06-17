3a5d4a5e390b62a21b998c5ae458c2d032b70966
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2020-06-17 19:32:53 -0300

    Inicio

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Component } from 'react';
+
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+     
+        <div className="App">
+          <h1>Inicio do projeto</h1>
+        </div>
+     
+    );
+
+  }
+
+}
+
+
+export default App;
---
+++ src/api/TaskService.js
@@ -0,0 +1,21 @@
+//mock
+class TaskService{
+    
+    constructor(){
+        this.tasks = [
+            {id:1,description:"Tarefa 1",whenToDo:"01/01/2030",done:false},
+            {id:2,description:"Tarefa 2",whenToDo:"02/01/2030",done:true},
+            {id:3,description:"Tarefa 3",whenToDo:"03/01/2030",done:false}
+        ]
+
+    }
+    list(){
+        return this.tasks;
+    }
+
+    delete(id){
+        this.tasks = this.tasks.filter(task=> task.id !== id);
+    }
+}
+
+export default new TaskService();
