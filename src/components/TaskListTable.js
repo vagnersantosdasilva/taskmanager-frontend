11fdae0acf345b5ce756621859eef7cab00e053a
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2020-06-18 14:50:46 -0300

    ligando tabela a servico mock e exibindo lista

--- src/components/TaskListTable.js
+++ src/components/TaskListTable.js
@@ -1,40 +1,71 @@
 import React, { Component } from 'react';
+import TaskService from '../api/TaskService';
 
 class TaskListTable extends Component {
+
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            tasks:[]
+        }
+    }
+
+    //Quando o componente estiver pronto
+    componentDidMount() {
+        this.listTasks();
+    }
+
+    //TODO:pegar a lista de tarefas do servico e colocar no  state local
+    listTasks(){
+        this.setState({tasks:TaskService.list()});
+    }
+
     render() {
         return (
-            <div className="container">
-                <table className="table">
-                    <TableHeader></TableHeader>
-                    <TableBody></TableBody>
+            <div className="container" style={{marginTop:20}}>
+                <table className="table table-striped">
+                    <TableHeader/>
+                    <TableBody tasks={this.state.tasks}/>
                 </table>
             </div>
         );
     }
 }
 
+
+
+
 const TableHeader = () =>{
     return (
-        <thead>
+        <thead className="thead-dark">
             <tr>
-                <th>Status</th>
-                <th>Descrição</th>
-                <th>Data</th>
-                <th>Acões</th>
+                <th scope="col">Status</th>
+                <th scope="col">Descrição</th>
+                <th scope="col">Data</th>
+                <th scope="col">Ações</th>
             </tr>
         </thead>
     )
 }
 
-const TableBody =() =>{
-return (
-    <tr> 
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td>4</td>
-    </tr>
-)
+const TableBody =(props) =>{
+    
+    return (
+        <tbody>
+            {props.tasks.map(task => 
+                <tr key={task.id}> 
+                    <td><input type="checkbox" checked={task.done}/></td>
+                    <td>{task.description}</td>
+                    <td>{task.whenToDo}</td>
+                    <td>
+                        <input type="button" value="Editar" className="btn btn-primary"/>&nbsp;
+                        <input type="button" value="Excluir" className="btn btn-danger"/>
+                    </td>
+                </tr>    
+            )}
+        </tbody>            
+    )
 }
 
 export default TaskListTable;
